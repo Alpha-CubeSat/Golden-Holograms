740db8580ff706af8b7502dc57a4f21b84e3ff45
Benjamin Shapiro <bps78@cornell.edu>
2021-07-05 17:31:21 -0400

    backend in progress

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
+
 app.listen(3000, ()=>console.log("listening at 3000"))
 app.use(express.static("public"));
+app.use(express.json()); //can add limits her in props to protect server
+
+
+app.post("/api", (request,response) => {
+  response.json({
+    "votestatus" : "successful!"
+  })
+  console.log(request.body);
+});
--- public/js/voting.js
+++ public/js/voting.js
@@ -3,11 +3,24 @@ $('.changeHologram').click(function(){
     $(this).addClass("active");
 });
 
+
+
 $('#man').click(function(){
     unityInstance.SendMessage('woman2', 'ChangeHologram', 'man');
     $('.vote').removeClass("visible");
     $('#vote-man').addClass("visible");
 
+});
+
+$('#vote-man').click(function(){
+  const manoptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"vote": "man"})
+  }
+  fetch("/api",manoptions);
 });
 
 $('#woman').click(function(){
@@ -16,10 +29,32 @@ $('#woman').click(function(){
     $('#vote-woman').addClass("visible");
 });
 
+$('#vote-woman').click(function(){
+  const womanoptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"vote": "woman"})
+  }
+  fetch("/api",womanoptions);
+});
+
 $('#moth').click(function(){
     unityInstance.SendMessage('woman2', 'ChangeHologram', 'moth');
     $('.vote').removeClass("visible");
     $('#vote-moth').addClass("visible");
+});
+
+$('#vote-moth').click(function(){
+  const mothoptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"vote": "moth"})
+  }
+  fetch("/api",mothoptions);
 });
 
 $('#dna').click(function(){
@@ -28,10 +63,32 @@ $('#dna').click(function(){
     $('#vote-dna').addClass("visible");
 });
 
+$('#vote-dna').click(function(){
+  const dnaoptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"vote": "dna"})
+  }
+  fetch("/api",dnaoptions);
+});
+
 $('#leopard').click(function(){
     unityInstance.SendMessage('woman2', 'ChangeHologram', 'leopard');
     $('.vote').removeClass("visible");
     $('#vote-leopard').addClass("visible");
+});
+
+$('#vote-leopard').click(function(){
+  const leopardoptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"vote": "leopard"})
+  }
+  fetch("/api",leopardoptions);
 });
 
 $('#cat').click(function(){
@@ -40,8 +97,30 @@ $('#cat').click(function(){
     $('#vote-cat').addClass("visible");
 });
 
+$('#vote-cat').click(function(){
+  const catoptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"vote": "cat"})
+  }
+  fetch("/api", catoptions);
+});
+
 $('#fish').click(function(){
     unityInstance.SendMessage('woman2', 'ChangeHologram', 'fish');
     $('.vote').removeClass("visible");
     $('#vote-fish').addClass("visible");
+});
+
+$('#vote-fish').click(function(){
+  const fishoptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"vote": "fish"})
+  }
+  fetch("/api",fishoptions);
 });
