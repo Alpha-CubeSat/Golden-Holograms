c6eda22d449d420ef2f1f24eb3583bbb6abceb14
Lin <lj233@cornell.edu>
2023-02-03 22:54:49 -0500

    add footer for chipsats page

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, ()=>console.log("listening at 3000"))
+app.listen(3000, () => console.log("listening at 3000"))
 app.use(express.static("public"));
 app.use(express.json()); //can add limits her in props to protect server
 
 
-app.post("/api", (request,response) => {
+app.post("/api", (request, response) => {
   response.json({
-    "votestatus" : "successful!"
+    "votestatus": "successful!"
   })
   console.log(request.body);
 });
+
+
+
