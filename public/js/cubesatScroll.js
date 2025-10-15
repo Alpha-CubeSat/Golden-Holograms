2445070e19e7c42de0b800d7bd8b581b06f3aef1
Grace <gtw25@cornell.edu>
2025-10-15 17:39:40 -0400

    speed up

--- public/js/cubesatScroll.js
+++ public/js/cubesatScroll.js
@@ -4,56 +4,60 @@ const currentFrame = index => (
   `images/cubesatScroll/animation${index.toString().padStart(3, '0')}.png`
 )
 
+// Preload all images into an array for instant access
+const images = [];
 const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
+  for (let i = 1; i <= frameCount; i++) {
     const img = new Image();
     img.src = currentFrame(i);
+    images[i] = img;
   }
 };
 
 preloadImages()
 
 window.onload = function () {
-  var previndex;
+  let prevIndex = 0;
+  let ticking = false;
   const html = document.documentElement;
   const canvas = (document.getElementsByClassName("cubsatScroll"))[0];
   const context = canvas.getContext("2d");
 
-  const img = new Image()
-  img.src = currentFrame(1);
-
   canvas.width = 947;
   canvas.height = 810;
 
-  var transparent = context.createImageData(947, 810);
-  for (var i = transparent.data.length; --i >= 0;)
-    transparent.data[i] = 0;
-
-  img.onload = function () {
-    context.drawImage(img, 0, 0);
+  // Draw initial frame
+  if (images[1] && images[1].complete) {
+    context.drawImage(images[1], 0, 0);
+  } else {
+    images[1].onload = () => {
+      context.drawImage(images[1], 0, 0);
+    };
   }
 
-  const updateImage = index => {
-    img.src = currentFrame(index);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(img, 0, 0);
-    //   window.requestAnimationFrame(() => updateImage(frameCount))
+  const updateImage = (index) => {
+    if (images[index] && images[index].complete) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(images[index], 0, 0);
+    }
+    ticking = false;
   }
 
-  window.addEventListener('scroll', () => {
+  const handleScroll = () => {
     const scrollTop = html.scrollTop;
     const maxScrollTop = window.innerHeight;
-    const scrollFraction = scrollTop / maxScrollTop;
+    const scrollFraction = Math.min(scrollTop / maxScrollTop, 1);
     const frameIndex = Math.min(
-      frameCount - 1,
-      Math.ceil(scrollFraction * frameCount),
+      frameCount,
+      Math.max(1, Math.ceil(scrollFraction * frameCount))
     );
 
-    if (frameIndex != previndex) {
-      requestAnimationFrame(() => updateImage(frameIndex + 1))
+    if (frameIndex !== prevIndex && !ticking) {
+      requestAnimationFrame(() => updateImage(frameIndex));
+      ticking = true;
+      prevIndex = frameIndex;
     }
+  };
 
-    previndex = frameIndex
-    console.log(frameIndex)
-  });
+  window.addEventListener('scroll', handleScroll, { passive: true });
 }
